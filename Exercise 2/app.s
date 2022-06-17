f979f1f76809ed3b66954573fc18349eb41f1a3c
Ignacio Martinez <itsigna02@gmail.com>
2022-06-17 10:52:10 -0300

    fix: add radar

--- Exercise 2/app.s
+++ Exercise 2/app.s
@@ -452,6 +452,67 @@ Registros: x1: X, x2: Y, x3: R
 	movk x10, 0x3A45, lsl 00
 	bl circle
 
+	movz x10, 0xA000, lsl 00
+	mov x3, 25	
+	bl circle
+	movz x10, 0xF000, lsl 00
+	mov x3, 23	
+	bl circle
+    movz x10, 0xA000, lsl 00
+	mov x3, 21	
+	bl circle
+    movz x10, 0xF000, lsl 00
+	mov x3, 19	
+	bl circle
+	movz x10, 0xA000, lsl 00
+	mov x3, 17	
+	bl circle
+	movz x10, 0xF000, lsl 00
+	mov x3, 15	
+	bl circle
+	movz x10, 0xA000, lsl 00
+	mov x3, 13	
+	bl circle
+	movz x10, 0xF000, lsl 00
+	mov x3, 11	
+	bl circle
+	movz x10, 0xA000, lsl 00
+	mov x3, 9	
+	bl circle
+	movz x10, 0xF000, lsl 00
+	mov x3, 7	
+	bl circle
+	movz x10, 0xA000, lsl 00
+	mov x3, 5	
+	bl circle
+	movz x10, 0xF000, lsl 00
+	mov x3, 3	
+	bl circle
+
+	//points in circle display
+
+	movz x1, 412
+	movz x2, 425
+	movz x3, 2
+	movz x4, 2
+    movz x10, #0x5000
+	bl rectangle
+	movz x1, 415
+	movz x2, 420
+	movz x3, 2
+	movz x4, 2
+	bl rectangle
+	movz x1, 420
+	movz x2, 430
+	movz x3, 2
+	movz x4, 2
+	bl rectangle
+	movz x1, 430
+	movz x2, 425
+	movz x3, 2
+	movz x4, 2
+	bl rectangle
+
 	// Bottom white circle trio
 	mov x1, 466		
 	mov x2, 308
